26c6dc5efac3607f40ef1bcae9470c114d72f115
Anil Hitang <anilhitang@Anils-MBP.phub.net.cable.rogers.com>
2021-05-19 18:25:23 -0300

    Minor fix on API's base url

--- redux/actions/countriesAction.js
+++ redux/actions/countriesAction.js
@@ -3,7 +3,7 @@ import * as covidApi from "../../generics/covidApi"
 const API_URL = process.env.API_URL
 
 export const fetchCountries = () => async (dispatch) => {
-  const response = await covidApi.get(`${API_URL}countries`)
+  const response = await covidApi.get(`${API_URL}/countries`)
   dispatch({
     type: types.GET_COUNTRIES,
     payload: response.data,
--- redux/actions/overviewAction.js
+++ redux/actions/overviewAction.js
@@ -3,7 +3,7 @@ import * as covidApi from "../../generics/covidApi"
 const API_URL = process.env.API_URL
 
 export const fetchOverview = () => async (dispatch) => {
-  const response = await covidApi.get(`${API_URL}all`)
+  const response = await covidApi.get(`${API_URL}/all`)
   dispatch({
     type: types.GET_OVERVIEW,
     payload: response.data,
